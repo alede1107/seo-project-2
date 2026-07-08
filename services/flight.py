f61305c56ca5be6f97eff0ed5bf26b850e25ebfd
alede1107 <alejandro.perdomo823@gmail.com>
2026-07-08 11:23:03 -0400

    finished flight.py, added airports.json

--- services/flight.py
+++ services/flight.py
@@ -1,2 +1,50 @@
 import requests
+import os
+from dotenv import load_dotenv
+import json
 
+path = os.path.join(os.path.dirname(__file__), "data", "airports.json")
+with open(path, "r", encoding="utf-8") as f:
+  airports = json.load(f)
+
+airports_by_iata = {
+  entry["iata"]: entry for entry in airports.values() if entry["iata"]
+  }
+
+load_dotenv()
+
+BASE_URL = "https://api.aviationstack.com/v1"
+TOKEN = os.getenv("AVIATIONSTACK_API_KEY")
+
+def get_destination_from_flight(flight_number):
+
+  flight_number = flight_number.strip().upper()
+
+  response = requests.get(f"{BASE_URL}/flights?access_key={TOKEN}&flight_iata={flight_number}")
+  data = response.json()
+
+  active_flight = None
+
+  for elem in data["data"]:
+    if elem["flight_status"] == "active":
+      active_flight = elem
+      break
+
+  if active_flight is None:
+    return {"success" : False, "reason" : "no_active_flight"}
+  
+  else:
+
+    airport = active_flight["arrival"]["iata"]
+    airport_info = airports_by_iata.get(airport)
+
+    if airport_info is None:
+      return {"success" : False, "reason" : "airport_not_in_database"}
+    
+    city = airport_info["city"]
+    
+    return {
+            "success" : True, 
+            "city" : city, 
+            "flight_status" : "active"
+            }
